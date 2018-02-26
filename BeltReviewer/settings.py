ee96a961e27ebfa044d69d239fc59807f178f360
Ubuntu <ubuntu@ip-172-31-43-206.us-east-2.compute.internal>
2018-02-26 03:45:59 +0000

    update

--- BeltReviewer/settings.py
+++ BeltReviewer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@b9+&r46ziab6q#n%=!_*8nzp5(4!(0lwbqr%!_q-xk*+t*$ja'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.219.231.22']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
